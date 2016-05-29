634b3f3fc651d1dde4be9475bb65c5b4c39e6eca
David Luu <luu.david.c@gmail.com>
2016-05-29 13:01:09 -0700

    Map sizing now relative to size of #map

--- public/js/map.js
+++ public/js/map.js
@@ -1,18 +1,28 @@
-var width = 960,
-    height = 600;
+var parentWidth = $('#map').width(),
+    parentHeight = window.innerHeight;
+
+var margin = 50,
+    width = parentWidth - (2 * margin),
+    height = parentHeight - (2 * margin);
+
+var svg = d3.select('#map').append('svg')
+          .attr("width", width)
+          .attr("height", height);
 
 queue()
-    .defer(d3.json, "/data/sd.json")
-    .defer(d3.json, "/data/ca-congress-114.json")
-    .await(ready);
+  .defer(d3.json, "/data/sd.json")
+  .defer(d3.json, "/data/ca-congress-114.json")
+  .await(ready);
 
 function ready(error, sd, congress) {
   if (error) throw error;
 
+  /* Retrieve features from the sd object */
   var sdgeo = topojson.feature(sd, sd.objects.land);
 
+  /* Calculate the center of sd, guess scale and offset */
   var center = d3.geo.centroid(sdgeo);
-  var scale = 25000;
+  var scale = 150;
   var offset = [width/2, height/2];
   var projection = d3.geo.mercator()
                       .scale(scale)
@@ -20,12 +30,25 @@ function ready(error, sd, congress) {
                       .translate(offset);
 
   var path = d3.geo.path()
+                .projection(projection);
+
+  var path = d3.geo.path()
     .projection(projection);
 
+  /* Use calculated results to fix offset and scale */
+  var bounds = path.bounds(sdgeo);
+  var hscale = scale*width  / (bounds[1][0] - bounds[0][0]);
+  var vscale = scale*height / (bounds[1][1] - bounds[0][1]);
+  var scale  = (hscale < vscale) ? hscale : vscale;
+  var offset  = [width + margin/2 - (bounds[0][0] + bounds[1][0])/2,
+                 height + margin/2 - (bounds[0][1] + bounds[1][1])/2];
 
-  var svg = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height);
+  projection = d3.geo.mercator()
+                  .center(center)
+                  .scale(scale)
+                  .translate(offset);
+
+  path = path.projection(projection);
 
   svg.append("defs").append("path")
       .attr("id", "land")
@@ -51,11 +74,6 @@ function ready(error, sd, congress) {
       .attr("class", "district-boundaries")
       .datum(topojson.mesh(congress, congress.objects.districts, function(a, b) { return a !== b && (a.id / 1000 | 0) === (b.id / 1000 | 0); }))
       .attr("d", path);
-
-/*  svg.append("path")
-      .attr("class", "state-boundaries")
-      .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
-      .attr("d", path);*/
 }
 
 d3.select(self.frameElement).style("height", height + "px");
