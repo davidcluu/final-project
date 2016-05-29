f62a9161005aabec02730ca27cb5ea7e61d365eb
David Luu <luu.david.c@gmail.com>
2016-05-29 13:58:38 -0700

    Map interactivity

--- public/js/map.js
+++ public/js/map.js
@@ -1,3 +1,7 @@
+/*
+ * Map
+ */
+
 var parentWidth = $('#map').width(),
     parentHeight = window.innerHeight;
 
@@ -6,12 +10,12 @@ var margin = 50,
     height = parentHeight - (2 * margin);
 
 var svg = d3.select('#map').append('svg')
-          .attr("width", width)
-          .attr("height", height);
+          .attr('width', width)
+          .attr('height', height);
 
 queue()
-  .defer(d3.json, "/data/sd.json")
-  .defer(d3.json, "/data/ca-congress-114.json")
+  .defer(d3.json, '/data/sd.json')
+  .defer(d3.json, '/data/ca-congress-114.json')
   .await(ready);
 
 function ready(error, sd, congress) {
@@ -50,30 +54,65 @@ function ready(error, sd, congress) {
 
   path = path.projection(projection);
 
-  svg.append("defs").append("path")
-      .attr("id", "land")
+  /* Define the clipping area */
+  svg.append('defs').append('path')
+      .attr('id', 'land')
       .datum(sdgeo)
-      .attr("d", path);
+      .attr('d', path);
 
-  svg.append("clipPath")
-      .attr("id", "clip-land")
-    .append("use")
-      .attr("xlink:href", "#land");
+  svg.append('clipPath')
+      .attr('id', 'clip-land')
+    .append('use')
+      .attr('xlink:href', '#land');
 
-  svg.append("g")
-      .attr("class", "districts")
-      .attr("clip-path", "url(#clip-land)")
-    .selectAll("path")
+  /* Draw political districts */
+  svg.append('g')
+      .attr('class', 'districts')
+      .attr('clip-path', 'url(#clip-land)')
+    .selectAll('path')
       .data(topojson.feature(congress, congress.objects.districts).features)
-    .enter().append("path")
-      .attr("d", path)
-    .append("title")
-      .text(function(d) { return d.id; });
+    .enter()
+      .append('path')
+        .attr('d', path)
+      .attr('class', function(d) { return 'district ' + d.id; })
+      .attr('title', function(d) { return d.id; })
+      .style('fill', district_defaultFill)
+      .attr('onmouseover', 'district_onMouseOver(this)')
+      .attr('onmouseout', 'district_onMouseOut(this)')
 
-  svg.append("path")
-      .attr("class", "district-boundaries")
+  /* Draw political district boundries */
+  svg.append('path')
+      .attr('class', 'district-boundaries')
       .datum(topojson.mesh(congress, congress.objects.districts, function(a, b) { return a !== b && (a.id / 1000 | 0) === (b.id / 1000 | 0); }))
-      .attr("d", path);
+      .attr('d', path);
 }
 
-d3.select(self.frameElement).style("height", height + "px");
+
+/*
+ * Interaction Functions
+ */
+
+var district_defaultFill = '#ddd'
+
+function district_onMouseOver(me) {
+  var $me = $(me)
+  var geoId = district_getGeoId($me)
+
+  $me.css('fill', 'cyan')
+}
+
+function district_onMouseOut(me) {
+  var $me = $(me)
+  var geoId = district_getGeoId($me)
+
+  $me.css('fill', district_defaultFill)
+}
+
+function district_getGeoId(obj) {
+  // obj.attr('class') = "district (district $)"
+  var geoid = obj.attr('class').substring(9)
+  var state = geoid.slice(0,-2)
+  var district = geoid.slice(-2)
+
+  return { state: parseInt(state), district: parseInt(district) }
+}
