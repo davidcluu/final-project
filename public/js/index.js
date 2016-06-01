e75219897c179d27521888732c6d6df37ba9eb03
Christine Do <christinevdo@gmail.com>
2016-05-31 17:57:35 -0700

    basic animation on scroll

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,28 @@
 $(document).ready(function() {
-   $("h1.hidden, h3.hidden, button.hidden").fadeIn('slow').removeClass("hidden");
+   //$("h1.fade, h3.fade, button.fade").addClass("animated fadeInUp");
+   var $window = $(window);
+   var $animate = $(".animate");
+
+   function checkInView() {
+      var windowHeight = $window.height();
+      var windowTop = $window.scrollTop();
+      var windowBottom = (windowHeight + windowTop);
+
+      $.each($animate, function() {
+         var $element = $(this);
+         var elementHeight = $element.outerHeight();
+         var elementTop = $element.offset().top;
+         var elementBottom = (elementHeight + elementTop);
+
+         if((elementBottom >= windowTop) && (elementTop <= windowBottom)) {
+            $element.addClass("animated fadeInUp");
+         }
+         else {
+            $element.removeClass("animate");
+         }
+      });
+   }
+
+   $window.on("scroll resize", checkInView);
+   $window.trigger("scroll");
 });
